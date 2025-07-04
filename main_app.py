a08ec350e9cc80ec9b47f4360115c3e5490cb3c4
鈴木啓太 <22311104ks@tama.ac.jp>
2025-07-05 00:18:25 +0900

    Change main_app file (07.05)

--- main_app.py
+++ main_app.py
@@ -4,16 +4,17 @@ from flask_survey_app.extensions import db
 from flask_survey_app.app import survey_bp
 
 def create_app():
-    app = Flask(__name__, template_folder='flask_survey_app/templates')
+    # 【変更】static_folderのパスを追加
+    app = Flask(
+        __name__, 
+        template_folder='flask_survey_app/templates',
+        static_folder='flask_survey_app/static' 
+    )
 
     # --- アプリケーションの設定 ---
     basedir = os.path.abspath(os.path.dirname(__file__))
-    
-    # 【変更】データベースファイルのパスを、データが格納されている
-    # 'flask_survey_app' フォルダ内の 'survey.db' を指すように修正します。
     survey_app_dir = os.path.join(basedir, 'flask_survey_app')
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(survey_app_dir, 'survey.db')
-    
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     # --- 拡張機能の初期化 ---
@@ -28,7 +29,6 @@ def create_app():
         return render_template('index.html')
 
     # --- データベースの作成 ---
-    # この処理は、もしDBファイルがなくてもエラーにならないようにするため、そのままにします。
     with app.app_context():
         db.create_all()
 
