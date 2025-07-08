9531dad9c437aa56e8ba0a513d3e531b941d50fe
鈴木啓太 <22311104ks@tama.ac.jp>
2025-07-08 13:37:38 +0900

    Update main_app file(07.08)

--- main_app.py
+++ main_app.py
@@ -6,8 +6,13 @@ from datetime import datetime
 from flask_survey_app.extensions import db
 from flask_survey_app.app import survey_bp
 from flask_survey_app.log_utils import Log
+from work_report_app.app import report_bp 
+from work_report_app import models
 
 def create_app():
+    """
+    Flaskアプリケーションのファクトリ関数
+    """
     app = Flask(
         __name__, 
         template_folder='flask_survey_app/templates',
@@ -17,13 +22,13 @@ def create_app():
     # --- アプリケーションの設定 ---
     basedir = os.path.abspath(os.path.dirname(__file__))
     survey_app_dir = os.path.join(basedir, 'flask_survey_app')
+    report_app_dir = os.path.join(basedir, 'work_report_app')
 
     # 1. アンケート用データベース (survey.db) の設定
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(survey_app_dir, 'survey.db')
-    
-    # 2. ログ用データベース (log.db) の設定
     app.config['SQLALCHEMY_BINDS'] = {
-        'logs': 'sqlite:///' + os.path.join(survey_app_dir, 'log.db')
+        'logs': 'sqlite:///' + os.path.join(survey_app_dir, 'log.db'),
+        'report': 'sqlite:///' + os.path.join(report_app_dir, 'report.db')
     }
     
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -33,25 +38,31 @@ def create_app():
 
     # --- Blueprintの登録 ---
     app.register_blueprint(survey_bp, url_prefix='/survey') 
+    app.register_blueprint(report_bp, url_prefix='/report')
     
     # --- ホームページのルート ---
     @app.route('/')
     def index():
+        """
+        ポータルトップページを表示
+        """
         return render_template('index.html')
 
+    # --- リクエスト後にログを記録する関数 ---
     @app.after_request
     def log_response_info(response):
-        # staticファイルへのリクエストはログの対象外にする
+        """
+        リクエストの情報をログデータベースに記録する
+        """
         if request.path.startswith('/static'):
             return response
-            
-        # 新しいLogオブジェクトを作成
+         
         new_log = Log(
             ip_address=request.remote_addr,
             path=request.path,
             method=request.method
         )
-        # データベースセッションに追加してコミット
+
         with app.app_context():
             db.session.add(new_log)
             db.session.commit()
@@ -61,11 +72,8 @@ def create_app():
     return app
 
 # --- アプリケーションの実行 ---
-# このファイルが直接実行された場合にのみ、サーバーを起動する
 if __name__ == '__main__':
     app = create_app()
     with app.app_context():
-        # データベースとテーブルを作成
         db.create_all()
-    # デバッグモードでアプリケーションを実行
     app.run(debug=True, host='0.0.0.0', port=5001)
